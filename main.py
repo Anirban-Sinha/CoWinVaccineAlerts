9a4cc43ee5894144442aaea778232aa851658034
Anirban-Sinha <60511828+Anirban-Sinha@users.noreply.github.com>
2021-06-30 20:16:16 +0530

    added exception block

--- main.py
+++ main.py
@@ -26,13 +26,17 @@ while True:
         return session["fee_type"] == "Free"
 
     def get_for_seven_days(start_date):
-        url = os.environ['REQUEST_URL2']+"?pincode={0}&date={1}".format(os.environ['PIN'],start_date.strftime("%d-%m-%Y"))
-        time.sleep(3)
-        #params = {"pincode": os.environ['PIN'], "date": start_date.strftime("%d-%m-%Y")}
-        headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:89.0) Gecko/20100101 Firefox/89.0"}
-        resp = requests.get(url, headers=headers)
-        data = resp.json()
-        return [session for session in get_sessions(data) if is_eighteen_plus(session) and is_available(session) and is_free(session)]
+        try:
+            url = os.environ['REQUEST_URL2']+"?pincode={0}&date={1}".format(os.environ['PIN'],start_date.strftime("%d-%m-%Y"))
+            time.sleep(3)
+            #params = {"pincode": os.environ['PIN'], "date": start_date.strftime("%d-%m-%Y")}
+            headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:89.0) Gecko/20100101 Firefox/89.0"}
+            resp = requests.get(url, headers=headers)
+            data = resp.json()
+            return [session for session in get_sessions(data) if is_eighteen_plus(session) and is_available(session) and is_free(session)]
+        except Exception:
+            print("something went wrong...restarting the program....")
+            handle_excp()
 
     def create_output(session_info):
         return f"{session_info['date']} - {session_info['name']} ({session_info['capacity']})"
@@ -52,3 +56,7 @@ while True:
         avi_req4 = requests.get(url="http://api.callmebot.com/text.php?source=web&user=@aruavidesh&text=Made%20with%20❤%20by%20Ani",headers=headers)
         req3 = requests.get(url=os.getenv('REQUEST_URL3'), headers=headers)
         req4 = requests.get(url="http://api.callmebot.com/text.php?source=web&user=@Anirban_Sinha&text=Made%20with%20❤%20by%20Ani",headers=headers)
+       
+    def handle_excp():
+        get_for_seven_days(start_date)
+        pass
