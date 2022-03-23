7d5ca473a069742d4a2e3356ff0678629d542092
Anirban Sinha <60511828+Anirban-Sinha@users.noreply.github.com>
2022-03-23 11:32:34 +0530

    Refactored Code

--- main.py
+++ main.py
@@ -58,14 +58,7 @@ while True:
             print("No availability")
         else:
             headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:89.0) Gecko/20100101 Firefox/89.0"}
-            avi_req3 = requests.get(url="http://api.callmebot.com/text.php?source=web&user=@aruavidesh&text=Vaccine%20Slots%20Available!!",headers=headers)
-            avi_req4 = requests.get(url="http://api.callmebot.com/text.php?source=web&user=@aruavidesh&text=Made%20with%20❤%20by%20Ani",headers=headers)
-            avida_req3 = requests.get(url="http://api.callmebot.com/text.php?source=web&user=@avisaga6&text=Vaccine%20Slots%20Available!!",headers=headers)
-            avida_req4 = requests.get(url="http://api.callmebot.com/text.php?source=web&user=@avisaga6&text=Made%20with%20❤%20by%20Ani",headers=headers)
             req3 = requests.get(url=os.getenv('REQUEST_URL3'), headers=headers)
-            req4 = requests.get(url="http://api.callmebot.com/text.php?source=web&user=@Anirban_Sinha&text=Made%20with%20❤%20by%20Ani",headers=headers)
-            avi_req2 = requests.get(url="http://api.callmebot.com/start.php?source=web&user=@aruavidesh&text=Vaccine%20slots%20available%20book%20fast.&lang=en-US-Standard-B",headers=headers)
-            avida_req2 = requests.get(url="http://api.callmebot.com/start.php?source=web&user=@avisaga6&text=Vaccine%20slots%20available%20book%20fast.&lang=en-US-Standard-B",headers=headers)
             req2 = requests.get(url=os.getenv('REQUEST_URL1'), headers=headers)
     except Exception:
         print("something went wrong...restarting the program....")
